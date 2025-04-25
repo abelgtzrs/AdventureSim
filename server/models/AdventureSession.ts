b987512aa12263d171366bba8423b1fc8802d4c9
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-04-25 12:15:08 -0400

    Changed Client side to TS and added Schema files

---
+++ server/models/AdventureSession.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Types, Document } from "mongoose";
+
+export interface IEntry {
+  prompt: string;
+  response: string;
+  chaosScore?: number;
+  timestamp: Date;
+}
+
+export interface IAdventureSession extends Document {
+  userId: Types.ObjectId;
+  title: string;
+  class: string;
+  isActive: boolean;
+  entries: IEntry[];
+}
+
+const entrySchema = new Schema<IEntry>({
+  prompt: String,
+  response: String,
+  chaosScore: Number,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const adventureSchema = new Schema<IAdventureSession>({
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  title: String,
+  class: String,
+  isActive: { type: Boolean, default: true },
+  entries: [entrySchema],
+});
+
+export default model<IAdventureSession>("AdventureSession", adventureSchema);
